b587b704fc5bc61f2bd8beacc7987fe06741b856
hussein <husseinpotter19@gmail.com>
2025-01-30 08:25:44 +0200

    checkpoint

--- lib/presentation/test.dart
+++ lib/presentation/test.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../core/theme/palette.dart';
 import '../core/widgets/buttons/custom_button.dart';
 import '../core/widgets/fields/custom_input.dart';
+import 'widgets/room_details_sheet.dart';
 
 class TestScreen extends StatelessWidget {
   const TestScreen({super.key});
@@ -11,24 +13,28 @@ class TestScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            1.sw.horizontalSpace,
-            CustomInput(
-              title: 'Email',
-              hint: 'Enter your email',
-              required: false,
-            ),
-            CustomButton(
-              text: 'heelo world',
-              onPressed: () {},
-            ),
-          ],
-        ),
+        child: RoomDetailsSheet(),
+        // child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        //   children: [
+        //     RoomDetailsSheet(),
+        //     1.sw.horizontalSpace,
+        //     CustomInput(
+        //       title: 'Email',
+        //       hint: 'Enter your email',
+        //       required: false,
+        //     ),
+        //     CustomButton(
+        //       text: 'heelo world',
+        //       onPressed: () {},
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
